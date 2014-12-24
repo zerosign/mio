535c4e4ae39d0f8b1c517b543ce61a95b20a7518
Steve Klabnik <steve@steveklabnik.com>
2014-12-24 22:00:07 +0000

    rebuild pages at ad527c5

--- implementors/collections/string/trait.IntoString.js
+++ implementors/collections/string/trait.IntoString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['mio'] = ["<a class='stability Unstable' title='Unstable: unsure about placement and naming'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/collections/string/trait.IntoString.html' title='collections::string::IntoString'>IntoString</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/ascii/struct.Ascii.html' title='std::ascii::Ascii'>Ascii</a>&gt;",];
+implementors['mio'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/collections/string/trait.IntoString.html' title='collections::string::IntoString'>IntoString</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/ascii/struct.Ascii.html' title='std::ascii::Ascii'>Ascii</a>&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
