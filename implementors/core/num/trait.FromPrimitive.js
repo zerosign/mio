08b04d15a67965fe04d2741243e0329ddbadbefe
Steve Klabnik <steve@steveklabnik.com>
2015-02-10 05:23:09 +0000

    rebuild pages at eadcdc9

--- implementors/core/num/trait.FromPrimitive.js
+++ implementors/core/num/trait.FromPrimitive.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors['nix'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='nix/errno/enum.Errno.html' title='nix::errno::Errno'>Errno</a>","<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='nix/errno/enum.Errno.html' title='nix::errno::Errno'>Errno</a>",];
-implementors['mio'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='nix/errno/consts/enum.Errno.html' title='nix::errno::consts::Errno'>Errno</a>",];
+implementors['mio'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for Errno",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
