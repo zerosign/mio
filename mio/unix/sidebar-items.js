c8d6c31f24289fcc3a2d05be04a63af38157a689
Steve Klabnik <steve@steveklabnik.com>
2015-03-12 22:25:36 +0000

    rebuild pages at 6493922

--- mio/unix/sidebar-items.js
+++ mio/unix/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["bind",""],["connect",""],["stream",""]],"struct":[["UnixListener",""],["UnixSocket",""],["UnixStream",""]]});
+initSidebarItems({"fn":[["bind",""],["connect",""],["stream",""]],"struct":[["Incoming","An iterator over incoming connections to a `UnixListener`."],["UnixListener",""],["UnixSocket",""],["UnixStream",""]]});
